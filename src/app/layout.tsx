16bc9e8130e5bbb5928671ee60f566e51c4b8cb9
qqqzhH <2902211465@qq.com>
2026-05-30 23:34:51 +0800

    fix: 修复左侧sidebar遮挡主内容区 -- 给 main 添加 md:ml-16

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           <Sidebar />
           <TabTitleTrick />
           <EasterEggs />
-          <main className="flex-1 min-w-0 w-full p-4 md:p-6 lg:p-8">
+          <main className="flex-1 min-w-0 w-full p-4 md:p-6 lg:p-8 md:ml-16">
             {children}
           </main>
         </div>
